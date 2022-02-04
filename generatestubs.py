fba6ea36f7099d6bbc41cb10f30d7a8a33422768
Alex Starche <alex.starche@ni.com>
2022-02-04 12:15:26 -0600

    Add grpc-device-dir option

--- generatestubs.py
+++ generatestubs.py
@@ -6,8 +6,14 @@ from pathlib import Path
 
 
 @click.command()
+@click.option(
+    "-d",
+    "--grpc-device-dir",
+    default="../grpc-device",
+    help="Root grpc-device directory to export to",
+)
 @click.argument("drivers", nargs=-1)
-def generate_stubs(drivers: Iterable[str]):
+def generate_stubs(grpc_device_dir: str, drivers: Iterable[str]):
     grpc_device_dir = Path("../grpc-device")
     proto_source_dir = grpc_device_dir / "source/protobuf"
     includes = [proto_source_dir] + [
